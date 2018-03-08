02f4751b8210aac253bc32c0aa881139807be135
Francisco Benavides <francisco.benavides@gmail.com>
2018-03-07 21:33:50 -0600

    Admin integration with database/tables creation and setup with admin user/role added

--- application/__init__.py
+++ application/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_security import Security
 from flask_security import SQLAlchemyUserDatastore
+from flask_security import utils
 from flask_mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
 from .models import db
@@ -58,7 +59,40 @@ def create_app():
     Bootstrap(app)
 
     # Setup Flask-Security
-    security = Security(app, SQLAlchemyUserDatastore(db, User, Role))
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security = Security(app, user_datastore)
+
+    # Executes before the first request is processed.
+    @app.before_first_request
+    def before_first_request():
+
+        # Create any database tables that don't exist yet.
+        db.create_all()
+
+        # Create the Roles "admin" and "end-user" -- unless they already exist
+        user_datastore.find_or_create_role(
+            name=app.config['ADMIN_USER'], description='Administrador')
+
+        # Create two Users for testing purposes -- unless they already exists.
+        # In each case, use Flask-Security utility function to encrypt the
+        # password.
+        encrypted_password = utils.encrypt_password(
+            app.config['ADMIN_PASSWORD'])
+        if not user_datastore.get_user(app.config['ADMIN_EMAIL']):
+            user_datastore.create_user(
+                email=app.config['ADMIN_EMAIL'], password=encrypted_password)
+
+        # Commit any database changes; the User and Roles must exist before
+        # we can add a Role to the User
+        db.session.commit()
+
+        # "admin" role. (This will have no effect if the Users already have
+        # these Roles.) Again, commit any database changes.
+        user_datastore.add_role_to_user(
+            app.config['ADMIN_EMAIL'], app.config['ADMIN_USER'])
+
+        # Commit the Admin User and Role
+        db.session.commit()
 
     # Debug = True to enable the toolbar
     toolbar = DebugToolbarExtension(app)
